2d50d951848756dd4379977b43fd8612810cade2
MitsuharuGoto <mh.goto.web@gmail.com>
2019-06-30 16:31:16 +0900

    投稿記事管理 更新処理作成

--- src/main/java/com/devlogmh/www/app/admin/post/PostDetailController.java
+++ src/main/java/com/devlogmh/www/app/admin/post/PostDetailController.java
@@ -3,6 +3,7 @@ package com.devlogmh.www.app.admin.post;
 import com.devlogmh.www.domain.admin.service.postDetail.PostDetailCreateService;
 import com.devlogmh.www.domain.admin.service.postDetail.PostDetailEditService;
 import com.devlogmh.www.domain.admin.service.postDetail.PostDetailService;
+import com.devlogmh.www.domain.admin.service.postDetail.PostDetailUpdateService;
 import com.devlogmh.www.domain.model.post.PostControlDto;
 import com.devlogmh.www.domain.model.post.PostForm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,10 +41,10 @@ public class PostDetailController {
     /** 編集画面 初期表示 */
     @Autowired
     private PostDetailEditService postDetailEditService;
-//
-//    /** 編集画面 更新処理 */
-//    @Autowired
-//    private CategoryDetailUpdateService categoryDetailUpdateService;
+
+    /** 編集画面 更新処理 */
+    @Autowired
+    private PostDetailUpdateService postDetailUpdateService;
 
     /**
      * 新規作成画面 表示
@@ -118,35 +119,35 @@ public class PostDetailController {
         return mav;
     }
 
-//    /**
-//     * 編集画面 更新処理
-//     */
-//    @PutMapping("edit/{id}")
-//    public ModelAndView update(@PathVariable Long id, @ModelAttribute("form") @Validated CategoryForm inputForm, BindingResult result, ModelAndView mav) {
-//
-//        // カテゴリーID
-//        postControlDto.setId(id);
-//        // カテゴリーマスタ詳細フォーム
-//        postControlDto.setCategoryForm(inputForm);
-//        // ModelAndView
-//        postControlDto.setMav(mav);
-//        // バリデーションエラー結果
-//        postControlDto.setBindingResult(result);
-//
-//        // 新規作成 登録処理
-//        categoryDetailUpdateService.delegate(postControlDto);
-//
-//        // エラーがあった場合
-//        if (result.hasErrors()) {
-//            // ビューの設定
-//            mav.setViewName(REDIRECT_CATEGORY_MASTER);
-//            return mav;
-//        }
-//
-//        // 更新後のリダイレクト先
-//        mav = new ModelAndView(RedirectContains.REDIRECT_CATEGORY_MASTER);
-//        return mav;
-//    }
+    /**
+     * 編集画面 更新処理
+     */
+    @PutMapping("edit/{id}")
+    public ModelAndView update(@PathVariable Long id, @ModelAttribute("form") @Validated PostForm inputForm, BindingResult result, ModelAndView mav) {
+
+        // カテゴリーID
+        postControlDto.setId(id);
+        // カテゴリーマスタ詳細フォーム
+        postControlDto.setPostForm(inputForm);
+        // ModelAndView
+        postControlDto.setMav(mav);
+        // バリデーションエラー結果
+        postControlDto.setBindingResult(result);
+
+        // 新規作成 登録処理
+        postDetailUpdateService.delegate(postControlDto);
+
+        // エラーがあった場合
+        if (result.hasErrors()) {
+            // ビューの設定
+            mav.setViewName(POST_EDIT);
+            return mav;
+        }
+
+        // 更新後のリダイレクト先
+        mav = new ModelAndView(REDIRECT_POST_LIST);
+        return mav;
+    }
 
 
 }
---
+++ src/main/java/com/devlogmh/www/domain/admin/service/postDetail/PostDetailUpdateService.java
@@ -0,0 +1,192 @@
+package com.devlogmh.www.domain.admin.service.postDetail;
+
+import com.devlogmh.www.domain.admin.service.common.AbsUtilService;
+import com.devlogmh.www.domain.admin.util.Contains;
+import com.devlogmh.www.domain.admin.util.SiteInfoUtil;
+import com.devlogmh.www.domain.admin.util.TimestampUtil;
+import com.devlogmh.www.domain.model.post.PostControlDto;
+import com.devlogmh.www.domain.model.post.PostDto;
+import com.devlogmh.www.domain.model.post.PostForm;
+import com.devlogmh.www.domain.model.session.SessionData;
+import com.devlogmh.www.exception.DuplicateProductException;
+import com.devlogmh.www.mapper.CategoryMapper;
+import com.devlogmh.www.mapper.PostMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@Transactional
+public class PostDetailUpdateService extends AbsUtilService {
+
+    @Autowired
+    private SessionData sessionData;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    // ------------- Mapper -----------------
+
+    /**
+     * 記事投稿管理マッパー
+     */
+    @Autowired
+    private PostMapper postMapper;
+
+    /**
+     * カテゴリーマッパー
+     */
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+
+    @Autowired
+    private PostControlDto postControlDto;
+
+    private PostForm postForm;
+
+    private BindingResult bindingResult;
+
+    private ModelAndView mav;
+
+    /**
+     * 初期処理
+     */
+    @Override
+    public void customInit() {
+        // コントローラーから渡された値を取得
+        this.postForm = postControlDto.getPostForm();
+        this.bindingResult = postControlDto.getBindingResult();
+        this.mav = postControlDto.getMav();
+    }
+
+    /**
+     * 主処理
+     */
+    @Override
+    public void mainProcess() {
+
+        // カテゴリーIDを取得
+        Long id = postControlDto.getId();
+
+        // フォームの初期設定
+        this.setupForm(this.postForm);
+
+        // エンティティ生成
+        PostDto postDto = new PostDto();
+
+        this.validate(id, this.postForm, this.bindingResult);
+
+        // エラーだった場合
+        if (this.bindingResult.hasErrors()) {
+            // オブジェクトを詰め込み
+            this.mav.addObject("form", this.postForm);
+            return;
+        }
+
+        // 更新する対象のIDを設定
+        postDto.setId(id);
+
+        // 更新処理
+        this.update(postDto, this.postForm);
+
+    }
+
+    /**
+     * フォームに入力された値を保持してフォームの初期設定
+     * @param inputForm
+     * @return
+     */
+    public void setupForm(PostForm inputForm) {
+
+        // rootPath
+        inputForm.setRootPath(SiteInfoUtil.getRootPath(request));
+        // URL
+        inputForm.setUrl(inputForm.getUrl());
+        // タイトル
+        inputForm.setTitle(inputForm.getTitle());
+        // キーワード
+        inputForm.setKeyword(inputForm.getKeyword());
+        // ディスクリプション
+        inputForm.setDescription(inputForm.getDescription());
+        // カテゴリーID
+        inputForm.setCategoryId(inputForm.getCategoryId());
+        // カテゴリー
+        inputForm.setCategoryList(inputForm.getCategoryList(categoryMapper));
+        // ステータスID
+        inputForm.setStatusId(inputForm.getStatusId());
+        // ステータス
+        inputForm.setStatusList(inputForm.getSelectedStatuses());
+        // コンテンツ
+        inputForm.setContent(inputForm.getContent());
+
+    }
+
+    /**
+     * 更新
+     */
+    public void update(PostDto postDto, PostForm inputForm) {
+
+        // URL
+        postDto.setUrl(inputForm.getUrl());
+        // タイトル
+        postDto.setTitle(inputForm.getTitle());
+        // キーワード
+        postDto.setKeyword(inputForm.getKeyword());
+        // ディスクリプション
+        postDto.setDescription(inputForm.getDescription());
+        // カテゴリーID
+        postDto.setCategoryId(inputForm.getCategoryId());
+        // ステータスID
+        postDto.setStatusId(inputForm.getStatusId());
+        // コンテンツ
+        postDto.setContent(inputForm.getContent());
+        // 更新者
+        postDto.setUpdaterId(sessionData.getUserId().longValue());
+        // 登録時間
+        postDto.setCreated(TimestampUtil.currentTime());
+        // 更新時間
+        postDto.setUpdated(TimestampUtil.currentTime());
+        // 削除フラグ
+        postDto.setDelflg(Contains.DelFlg.NOT_DEL.getValue());
+
+        postMapper.update(postDto);
+    }
+
+    /**
+     * 更新 バリデーションチェック
+     * @param postId, inputForm
+     * @throws DuplicateProductException
+     */
+    public void validate(Long postId, PostForm inputForm, BindingResult result) {
+
+        // 編集中のデータ
+        PostDto editData = postMapper.select(postId);
+
+        // ユーザー管理エンティティから検索
+        List<PostDto> dtoList = postMapper.selectAll();
+
+        for (PostDto dto: dtoList) {
+
+            // URLの重複チェック
+            if (StringUtils.equals(dto.getUrl(), inputForm.getUrl())
+                    && !StringUtils.equals(editData.getUrl(), inputForm.getUrl())) {
+                result.rejectValue("url", "duplicate", new String[]{"URL"}, "default message.");
+            }
+
+        }
+
+        if (Objects.nonNull(result.getErrorCount())) {
+            new DuplicateProductException();
+        }
+
+    }
+
+}
