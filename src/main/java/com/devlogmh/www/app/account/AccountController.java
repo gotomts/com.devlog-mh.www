900ce2c128d23f59f5d6aec09bef74f1965b46a9
MitsuharuGoto <mh.goto.web@gmail.com>
2019-07-15 10:35:46 +0900

    初期アカウント作成用のコントローラーをコメントアウト

--- src/main/java/com/devlogmh/www/app/account/AccountController.java
+++ src/main/java/com/devlogmh/www/app/account/AccountController.java
@@ -1,81 +1,81 @@
-package com.devlogmh.www.app.account;
-
-import com.devlogmh.www.domain.model.account.AccountEntity;
-import com.devlogmh.www.domain.model.account.AccountForm;
-import com.devlogmh.www.domain.service.account.AccountService;
-import com.devlogmh.www.domain.admin.util.Contains;
-import com.devlogmh.www.domain.admin.util.TimestampUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-public class AccountController {
-
-    @Autowired
-    private AccountService accountService;
-
-    /**
-     * フォームに入力された値を取得するクラスを作成します。
-     * @return
-     */
-    @ModelAttribute
-    public AccountForm setupForm() {
-        return new AccountForm();
-    }
-
-    /**
-     * accountで接続した場合に表示するHTMLを設定します.
-     * @return 表示するHTML
-     */
-    @RequestMapping(value="account")
-    String accountForm() {
-        return "account/accountForm";
-    }
-
-    /**
-     * アカウントを作成
-     * @param form
-     * @param bindingResult
-     * @return
-     */
-    @RequestMapping(value = "account", method = RequestMethod.POST)
-    String create(@Validated AccountForm form, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "account/accountForm";
-        }
-        // ユーザーエンティティをインスタンス化
-        AccountEntity accountEntity = new AccountEntity();
-        // ユーザー名を設定
-        accountEntity.setName(form.getName());
-        // メールアドレスを設定
-        accountEntity.setEmail(form.getEmail());
-        // パスワードをbcrypt方式でハッシュ化して設定
-        accountService.create(accountEntity, form.getPassword());
-        // 権限を設定
-        accountEntity.setRoleId(Integer.parseInt(Contains.RoleId.ADMIN.getValue()));
-        // 更新者を設定
-        accountEntity.setUpdaterId(1);
-        // 作成日時を設定
-        accountEntity.setCreated(TimestampUtil.currentTime());
-        // 更新日時を設定
-        accountEntity.setUpdated(TimestampUtil.currentTime());
-        // 削除フラグを設定
-        accountEntity.setDelflg(Contains.DelFlg.NOT_DEL.getValue());
-        return "redirect:/account/complete";
-    }
-
-    /**
-     * アカウント作成に整合した場合に遷移します。
-     * @return
-     */
-    @RequestMapping(value = "account/complete", method = RequestMethod.GET)
-    String createFinish() {
-        return "account/accountForm";
-    }
-
-}
+//package com.devlogmh.www.app.account;
+//
+//import com.devlogmh.www.domain.model.account.AccountEntity;
+//import com.devlogmh.www.domain.model.account.AccountForm;
+//import com.devlogmh.www.domain.service.account.AccountService;
+//import com.devlogmh.www.domain.admin.util.Contains;
+//import com.devlogmh.www.domain.admin.util.TimestampUtil;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//@Controller
+//public class AccountController {
+//
+//    @Autowired
+//    private AccountService accountService;
+//
+//    /**
+//     * フォームに入力された値を取得するクラスを作成します。
+//     * @return
+//     */
+//    @ModelAttribute
+//    public AccountForm setupForm() {
+//        return new AccountForm();
+//    }
+//
+//    /**
+//     * accountで接続した場合に表示するHTMLを設定します.
+//     * @return 表示するHTML
+//     */
+//    @RequestMapping(value="account")
+//    String accountForm() {
+//        return "account/accountForm";
+//    }
+//
+//    /**
+//     * アカウントを作成
+//     * @param form
+//     * @param bindingResult
+//     * @return
+//     */
+//    @RequestMapping(value = "account", method = RequestMethod.POST)
+//    String create(@Validated AccountForm form, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//            return "account/accountForm";
+//        }
+//        // ユーザーエンティティをインスタンス化
+//        AccountEntity accountEntity = new AccountEntity();
+//        // ユーザー名を設定
+//        accountEntity.setName(form.getName());
+//        // メールアドレスを設定
+//        accountEntity.setEmail(form.getEmail());
+//        // パスワードをbcrypt方式でハッシュ化して設定
+//        accountService.create(accountEntity, form.getPassword());
+//        // 権限を設定
+//        accountEntity.setRoleId(Integer.parseInt(Contains.RoleId.ADMIN.getValue()));
+//        // 更新者を設定
+//        accountEntity.setUpdaterId(1);
+//        // 作成日時を設定
+//        accountEntity.setCreated(TimestampUtil.currentTime());
+//        // 更新日時を設定
+//        accountEntity.setUpdated(TimestampUtil.currentTime());
+//        // 削除フラグを設定
+//        accountEntity.setDelflg(Contains.DelFlg.NOT_DEL.getValue());
+//        return "redirect:/account/complete";
+//    }
+//
+//    /**
+//     * アカウント作成に整合した場合に遷移します。
+//     * @return
+//     */
+//    @RequestMapping(value = "account/complete", method = RequestMethod.GET)
+//    String createFinish() {
+//        return "account/accountForm";
+//    }
+//
+//}
